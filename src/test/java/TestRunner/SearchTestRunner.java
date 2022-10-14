18c43b59e4a98fdc15d424f251487eb2e14bd2d4
Meheedi-Hasan <mahdyhassann@gmail.com>
2022-10-14 21:49:36 +0600

    made some changes2

--- src/test/java/TestRunner/SearchTestRunner.java
+++ src/test/java/TestRunner/SearchTestRunner.java
@@ -25,37 +25,28 @@ public class SearchTestRunner extends Setup {
         loginPage.login(utils.getEmail(), utils.getPassword());
 
     }
+
     @Test(priority = 5)
     public void doSearch() throws InterruptedException {
         searchPage = new SearchPage(driver);
         String text2 = searchPage.doSearch();
-        Assert.assertEquals(text2,"Colorful Polo Shirt for Men-RB (E)");
+        Assert.assertEquals(text2, "Colorful Polo Shirt for Men-RB (E)");
     }
+
     @Test(priority = 6)
     public void addToCartWithZeroInput() throws InterruptedException {
 
         searchPage = new SearchPage(driver);
         String txt = searchPage.addToCartWithZeroInput();
-        System.out.println(txt);
-//        try {
-//
-//
-//        }
-//        catch (Exception e){
-//            e.printStackTrace();
-//        }
-    }
- //   @Test(priority = 7)
- //   public void addToCartWithValidInput() throws InterruptedException {
- //       searchPage = new SearchPage(driver);
- //       String txt = searchPage.addToCartWithValidInput();
- //       Assert.assertEquals(txt,"Tk 2,000.00");
-//        try {
-//
-//
-//        }
-//        catch (Exception e){
-//            e.printStackTrace();
-//        }
+        Assert.assertEquals(txt, "Quantity should be positive");
+
     }
 
+    @Test(priority = 7)
+    public void addToCartWithValidInput() throws InterruptedException {
+        searchPage = new SearchPage(driver);
+        String txt = searchPage.addToCartWithValidInput();
+        Assert.assertEquals(txt, "2");
+    }
+
+}
--- src/test/java/pages/SearchPage.java
+++ src/test/java/pages/SearchPage.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
 
 public class SearchPage {
     WebDriver driver;
@@ -26,8 +25,8 @@ public class SearchPage {
     public WebElement cartBtn;
     @FindBy(xpath = "//a[@class='cart-trigger']")
     public WebElement cartBox;
-    @FindBy(xpath = "//span[@class='product-subtotal']")
-    public WebElement totalPrice;
+//    @FindBy(xpath = "//span[@class='product-subtotal']")
+//    public WebElement totalPrice;
 
     @FindBy(xpath = "//input[contains(@class,'qty-input valid')]")
     public WebElement qtybox2;
@@ -54,48 +53,33 @@ public class SearchPage {
 
     public String addToCartWithZeroInput() throws InterruptedException {
         imgBox.click();
-
-        radioSizeBtn.click();
-
-        qtyBox.clear();
-
-        qtyBox.sendKeys("0");
-
-        cartBtn.click();
-        Thread.sleep(3000);
-
-        Alert alert = driver.switchTo().alert();
         Thread.sleep(2000);
+        radioSizeBtn.click();
+        Thread.sleep(2000);
+        qtyBox.clear();
+        Thread.sleep(2000);
+        qtyBox.sendKeys("0");
+        Thread.sleep(2000);
+        cartBtn.click();
+        Thread.sleep(2000);
+        Alert alert = driver.switchTo().alert();
         String alertMessage = driver.switchTo().alert().getText();
-        Assert.assertEquals(alertMessage, "Quantity should be positive");
+        //Assert.assertEquals(alertMessage, "Quantity should be positive");
         alert.accept();
-        Thread.sleep(3000);
+        return alertMessage;
+
+    }
+    public String addToCartWithValidInput() throws InterruptedException {
 
         qtybox2.clear();
-        // qtyBox.click();
-        // qtyBox.clear();
-        // Thread.sleep(2000);
+        Thread.sleep(2000);
         qtybox2.sendKeys("2");
-
+        Thread.sleep(2000);
         cartBtn.click();
-
+        Thread.sleep(2000);
         cartBox.click();
         Thread.sleep(4000);
         String qty = qtyasrt.getText();
         return qty;
-
     }
-//    public String addToCartWithValidInput() throws InterruptedException {
-//        //radioSizeBtn.click();
-//        Thread.sleep(2000);
-//        qtyBox.clear();
-//        Thread.sleep(2000);
-//        qtyBox.sendKeys("2");
-//        Thread.sleep(2000);
-//        cartBtn.click();
-//        Thread.sleep(2000);
-//        cartBox.click();
-//        String txt = totalPrice.getText();
-//        return txt;
-//    }
 }
